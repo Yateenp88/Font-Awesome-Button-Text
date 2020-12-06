c54c85691478a69a482cef6ecee902b247b96b8f
yateen <ypdowork@gmail.com>
2020-12-06 17:42:13 +0530

    uploaded

---
+++ icon-test1.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<link rel="shortcut icon" href="img/ielogo.png">
+    <title>ITA | Mac/Linux Asset</title>
+
+	<!-- Font free icon start--->
+	<link rel="stylesheet" href="css/fontawesome-free-5.6.3/css/font-awesome.css">
+	<link rel="stylesheet" href="css/fontawesome-free-5.6.3/css/all.css">
+	<!-- Font free icon end--->		
+
+  <span id="bar-font-blk" ><i class="fas fa-desktop"></i>&nbsp;&nbsp;Mac/Linux Master</span>
+ <span id="bar-font-blk" ><i class="fas fa-desktop"></i>&nbsp;&nbsp;Mac/Linux Master</span>
+ <span id="bar-font-blk" ><i class="fas fa-tv"></i>&nbsp;&nbsp;Mac/Linux Master</span>
+ 
+ 
+ <div class="fa-3x">
+  <i class="fas fa-hourglass fa-spin"></i>
+  <i class="fas fa-circle-notch fa-spin"></i>
+  <i class="fas fa-sync fa-spin"></i>
+  <i class="fas fa-cog fa-spin"></i>
+  <i class="fas fa-spinner fa-pulse"></i>
+  <i class="fas fa-stroopwafel fa-spin"></i>
+</div>
+ 
+ 
+ <span class="fa-stack fa-2x">
+  <i class="fas fa-square fa-stack-2x" style="color:red"></i>
+  <i class="fab fa-twitter fa-stack-1x fa-inverse" ></i>
+</span>
+
+ <span class="fa-stack fa-2x">
+  <i class="fas fa-square fa-stack-2x" style="color:blue"></i>
+  <i class="fab fa-twitter fa-stack-1x fa-inverse" ></i>
+</span>
+<span class="fa-stack fa-2x">
+  <i class="fas fa-circle fa-stack-2x"></i>
+  <i class="fas fa-flag fa-stack-1x fa-inverse" style="color:red"></i>
+</span>
+<span class="fa-stack fa-2x">
+  <i class="fas fa-square fa-stack-2x"></i>
+  <i class="fas fa-terminal fa-stack-1x fa-inverse"></i>
+</span>
+<span class="fa-stack fa-4x">
+  <i class="fas fa-square fa-stack-2x"></i>
+  <i class="fas fa-terminal fa-stack-1x fa-inverse" ></i>
+</span>
+<span class="fa-stack fa-2x"><i class="fas fa-camera fa-stack-1x"></i><i class="fas fa-ban fa-stack-2x" style="color:Tomato"></i</span>
+<span class="fa-stack fa-2x">
+  <i class="fas fa-lightbulb fa-stack-1x"></i>
+  <i class="fas fa-ban fa-stack-2x" style="color:orange"></i>
+</span>
+
+
+
+
+
+<i class="fas fa-stroopwafel fa-xs"></i>
+<i class="fas fa-stroopwafel fa-sm"></i>
+<i class="fas fa-stroopwafel fa-lg"></i>
+<i class="fas fa-stroopwafel fa-2x"></i>
+<i class="fas fa-stroopwafel fa-3x"></i>
+<i class="fas fa-stroopwafel fa-5x"></i>
+<i class="fas fa-stroopwafel fa-7x"></i>
+<i class="fas fa-stroopwafel fa-10x"></i>
+<i class="fas fa-stroopwafel fa-20x"></i>
+<i class="fas fa-stroopwafel fa-30x"></i>
+ 
+ 
+</body>
+
+</html>
